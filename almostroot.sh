0fbae81e9684f7ec26ca15b71b87480fe6a5ece0
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-10-08 14:53:57 -0300

    almostroot.sh

---
+++ almostroot.sh
@@ -0,0 +1,5 @@
+echo "Tentando conseguir permissões... (Apenas funciona no Termux, Android!)"
+chmod -R 777 /data/data/com.termux/files/home
+chmod -R 777 *
+chmod +x *
+chmod +x /data/data/com.termux/files/home
